71e3f424a3487a461b221983fab51c65fc4af7aa
Steven Ramkumar <steveram@gmail.com>
2011-01-18 23:25:03 -0800

    server side clustering instead of client side. First step towards porting this to iphone.

--- webserver.js
+++ webserver.js
@@ -12,7 +12,8 @@ var sys = require('sys'),
     storage = require('./storage.js'),
     foo = require("./server"),
     url = require("url"),
-    qs = require("querystring")
+    qs = require("querystring"),
+    cluster = require("./cluster");
 
 GRANULARITY = "neighborhood";
 
@@ -20,6 +21,8 @@ GRANULARITY = "neighborhood";
 foo.listen(Number(process.env.PORT || PORT), HOST);
 
 foo.get("/", foo.staticHandler("index.html"));
+foo.get("/cluster", foo.staticHandler("cluster.html"));
+
 
 foo.get("/hotspots", function (req, res) {
     var swLat = parseFloat(qs.parse(url.parse(req.url).query).swLat),
@@ -28,8 +31,6 @@ foo.get("/hotspots", function (req, res) {
         neLng = parseFloat(qs.parse(url.parse(req.url).query).neLng);
 
     storage.getTweetsWithinViewPortInLast3Hours('tweets', {swLat: swLat, swLng: swLng, neLat: neLat, neLng: neLng}, function(data){
-        sys.puts(data.length);
-        sys.puts(typeof data[0]);
         res.simpleJSON(200, {data: data, rss: mem.rss}); 
     });
 });
@@ -39,7 +40,21 @@ foo.get("/gettweet", function(req, res){
         storage.getTweet('tweets', {id: id}, function(data){
         	res.simpleJSON(200, {data: data, rss: mem.rss}); 
     	});
-})
+});
+
+foo.get("/clusters", function(req, res){
+    var swLat = parseFloat(qs.parse(url.parse(req.url).query).swLat),
+        swLng = parseFloat(qs.parse(url.parse(req.url).query).swLng),
+        neLat = parseFloat(qs.parse(url.parse(req.url).query).neLat),
+        neLng = parseFloat(qs.parse(url.parse(req.url).query).neLng),
+        zoom = parseFloat(qs.parse(url.parse(req.url).query).zoom),
+        clusters;
+	
+	storage.getTweetsWithinViewPortInLast3Hours('tweets', {swLat: swLat, swLng: swLng, neLat: neLat, neLng: neLng}, function(data){
+		clusters = cluster.getClusters(data, zoom);
+        res.simpleJSON(200, {data: clusters, rss: mem.rss}); 
+    });
+});
 
 foo.get("/images/tweet.png", foo.staticHandler("assets/images/tweet.png"));
 foo.get("/images/foursquare.png", foo.staticHandler("assets/images/foursquare.png"));
